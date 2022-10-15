cb697c2841b954ca739a1ca4373ec5a454332500
ACF <alricfernandes11@gmail.com>
2022-10-15 13:34:27 -0400

    update file with spacing of prompt.

--- spinner1.js
+++ spinner1.js
@@ -11,27 +11,27 @@ setTimeout(() => {
 }, 500);
 
 setTimeout(() => {
-  process.stdout.write('\r\\  ');
+  process.stdout.write('\r\\   ');
 }, 700);
 
 setTimeout(() => {
-  process.stdout.write('\r|  ');
+  process.stdout.write('\r|   ');
 }, 900);
 
 setTimeout(() => {
-  process.stdout.write('\r/  ');
+  process.stdout.write('\r/   ');
 }, 1100);
 
 setTimeout(() => {
-  process.stdout.write('\r-  ');
+  process.stdout.write('\r-   ');
 }, 1300);
 
 setTimeout(() => {
-  process.stdout.write('\r\\  ');
+  process.stdout.write('\r\\   ');
 }, 1500);
 
 setTimeout(() => {
-  process.stdout.write('\r|  ');
+  process.stdout.write('\r|   ');
 }, 1700);
 
 setTimeout(() => {
